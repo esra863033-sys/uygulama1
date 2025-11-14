3943d4974dc35496d7e2a8d315ff2d9594fbcefd
esra863033-sys <esra863033@gmail.com>
2025-11-14 13:03:24 +0300

    Update web_service.py

--- web_service.py
+++ web_service.py
@@ -34,53 +34,54 @@ def index():
 
             mesajlar.append({"isim": isim, "mesaj": mesaj})
 
-    return redirect(url_for("index"))
+        return redirect(url_for("index"))
 
 
 # sadece yetkili görsün:
 
-show_secret = session.get("admin", False)
+    show_secret = session.get("admin", False)
 
-return render_template("index.html", mesajlar=mesajlar, secret_message=SECRET_MESSAGE if show_secret else None, is_admin=show_secret)
-
-
-@app.route("/login", methods=["GET", "POST"])
-
-def login():
-
-    if request.method == "POST":
-
-        pwd = request.form.get("password")
-
-        if pwd and pwd == ADMIN_PASSWORD:
-
-            session["admin"] = True
-
-            flash("Başarıyla giriş yapıldı.", "success")
-
-            return redirect(url_for("index"))
-
-    else:
-
-        flash("Hatalı parola.", "danger")
-
-        return redirect(url_for("login"))
-
-    return render_template("login.html")
+    return render_template("index.html", mesajlar=mesajlar, secret_message=SECRET_MESSAGE if show_secret else None, is_admin=show_secret)
 
 
-    @app.route("/logout")
+    @app.route("/login", methods=["GET", "POST"])
 
-    def logout():
+    def login():
 
-        session.pop("admin", None)
+        if request.method == "POST":
 
-        flash("Çıkış yapıldı.", "info")
+            pwd = request.form.get("password")
 
-        return redirect(url_for("index"))
+            if pwd and pwd == ADMIN_PASSWORD:
+
+                session["admin"] = True
+
+                flash("Başarıyla giriş yapıldı.", "success")
+
+                return redirect(url_for("index"))
+
+            else:
+
+                flash("Hatalı parola.", "danger")
+
+                return redirect(url_for("login"))
+
+               return render_template("login.html")
 
 
-    if __name__ == "__main__":
+                @app.route("/logout")
 
-        app.run(host="0.0.0.0", port=5000)
+                def logout():
+                    
+
+                    session.pop("admin", None)
+
+                    flash("Çıkış yapıldı.", "info")
+
+                    return redirect(url_for("index"))
+
+
+                if __name__ == "__main__":
+
+                    app.run(host="0.0.0.0", port=5000)
 
